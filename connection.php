2129e661095ca0a7c4d2413b6ba3e7127f08feff
FranciscoJRamirez <42820012+FranciscoJRamirez@users.noreply.github.com>
2020-04-22 21:27:15 -0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,6 +5,7 @@ $db= 'mixfy';
 $username  = 'root';
 $passworrd = '';
 
+$connection = new mysqli($hostname, $username, $passworrd, $db,3308 );
 
 if($connection->connect_errno){
     echo 'Server does not work ';
